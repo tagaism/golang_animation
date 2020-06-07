350f917f7b1f980f1938b1a9407a5447f03d17cb
tagaism <i77tag@gmail.com>
2020-06-08 02:24:50 +0900

    Acceleration towards mouse

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Animation with Go
-Inspired by <a href="https://natureofcode.com/">Nature of code</a>
+Inspired by <a href="https://twitter.com/shiffman">Daniel Shiffman</a> and his book <a href="https://natureofcode.com/">Nature of code</a>
 ### Bouncing balls<br/>
 Created bunch of random balls by vectorizing location and velocity<br/>
 ![](bouncing_balls/bouncing_balls.gif)
+
+### Balls acceleration towards mouse<br/>
+![](acceleration_towards_mouse/acceleration_m.gif)
--- acceleration_towards_mouse/acceleration_towards_mouse.go
+++ acceleration_towards_mouse/acceleration_towards_mouse.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/h8gi/canvas"
 	"math/rand"
+	"math"
 )
 
 type Vector struct {
@@ -13,6 +14,7 @@ type Vector struct {
 type Ball struct {
 	location Vector
 	velocity Vector
+	acceleration Vector
 	radius float64
 }
 
@@ -30,22 +32,24 @@ func main() {
 	})
 
 	balls := []Ball{}
-	for i := 0; i < 10; i++ {
+	for i := 0; i < 3; i++ {
 		r := randFloat(0, 80)
 		b := Ball{
 			location : Vector{randFloat(r, WIDTH-r), randFloat(r, HEIGHT-r)},
-			velocity : Vector{randFloat(-5, 5), randFloat(-5, 5)},
+			velocity : Vector{0, 0},
+			acceleration : Vector{0, 0},
 			radius : r,
 		}
 		balls = append(balls, b)
 	}
 	c.Draw(func(ctx *canvas.Context) {
+		mouse := Vector{ctx.Mouse.X, ctx.Mouse.Y}
 		ctx.SetRGB(1, 1, 1)
 		ctx.Clear()
 		ctx.Push()
 		for i := 0; i < len(balls); i++ {
 			balls[i].DrawBall(ctx)
-			balls[i].Move()
+			balls[i].Move(mouse)
 			balls[i].Edge()
 		}
 		ctx.Pop()
@@ -56,6 +60,31 @@ func main() {
 func (a *Vector) Add(b Vector) {
 	(*a).x += b.x
 	(*a).y += b.y
+}
+
+func (a *Vector) Sub(b Vector) {
+	(*a).x -= b.x
+	(*a).y -= b.y
+}
+
+func (a *Vector)Limit(max float64) {
+	if (*a).x > max {
+		(*a).x = max
+	}
+	if (*a).y > max {
+		(*a).y = max
+	}
+}
+
+func (a *Vector) Normalize() {
+	mag := math.Sqrt(a.x * a.x + a.y * a.y)
+	(*a).x = (*a).x / mag
+	(*a).y = (*a).y / mag
+}
+
+func (a *Vector) Mult(v float64) {
+	(*a).x *= v
+	(*a).y *= v
 }
 
 // Balls logic
@@ -70,15 +99,29 @@ func (b *Ball) DrawBall(ctx *canvas.Context) {
 	ctx.SetLineWidth(1)
 }
 
-func (b *Ball) Move() {
+func (b *Ball) Move(point Vector) {
+	point.Sub(b.location)
+
+	//set point vector magnitude
+	point.Normalize()
+	point.Mult(0.5)
+
+	b.acceleration = point
+	b.velocity.Add(b.acceleration)
+	b.velocity.Limit(5)
 	b.location.Add(b.velocity)
 }
 
 func (b *Ball) Edge() {
-	if b.location.x < b.radius || b.location.x > WIDTH - b.radius {
-		b.velocity.x *= -1
+	if b.location.x > WIDTH {
+		b.location.x = 0
+	} else if b.location.x < 0 {
+		b.location.x = WIDTH
 	}
-	if b.location.y < b.radius || b.location.y > HEIGHT - b.radius {
-		b.velocity.y *= -1
+
+	if b.location.y > HEIGHT {
+		b.location.y = 0
+	} else if b.location.y < 0 {
+		b.location.y = HEIGHT
 	}
 }
